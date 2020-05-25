e99acd5872f6272313de1efea8e05b70d429da5c
eranedri <eran.edri@apester.com>
2020-05-25 17:54:15 +0300

    test

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import os
 
+start_date = '20200501'
+end_date = '20200505'
 
+table_route = 'aggs_v2.creation_measures' + "$" + start_date
+commend_template = 'bq rm -f '
+# def main():
 
-def main():
+#  first stage we gonna delete partitions with for loop according to calender inputs.
 
-    os.system("pwd")
+print(commend_template + table_route)
+os.system("pwd")
 
+# /// second stage we upload new data from side "view" table 
