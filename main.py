f8d4a1ba2c24818f9e1d8e2485b1052bb893b34c
eranedri <eran.edri@apester.com>
2020-05-25 18:10:20 +0300

    test

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import os
+from datetime import timedelta, date
 
-start_date = '20200501'
-end_date = '20200505'
-
-table_route = 'aggs_v2.creation_measures' + "$" + start_date
+start_date = date(2020, 5, 1)
+end_date = date(2020, 5, 5)
+table_route = 'aggs_v2.creation_measures$' 
 commend_template = 'bq rm -f '
-# def main():
 
-#  first stage we gonna delete partitions with for loop according to calender inputs.
+def daterange(start_date, end_date):
+    for n in range(int ((end_date - start_date).days)):
+         yield start_date + timedelta(n)
 
-print(commend_template + table_route)
-os.system("pwd")
+def main():
 
-# /// second stage we upload new data from side "view" table 
+#first stage we gonna delete partitions with for loop according to calender inputs.
+
+    for single_date in daterange(start_date, end_date):
+        print(commend_template + table_route + single_date.strftime("%Y%m%d"))
+        # os.system("pwd")
+
+#second stage we upload new data from side "view" table 
+
+
+
+if __name__ == "__main__":
+    main()
+
