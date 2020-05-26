9e1529ddde58004f17224b1d797e2928457801ee
eranedri <eran.edri@apester.com>
2020-05-26 11:57:08 +0300

    finish first stage

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import os
 from datetime import timedelta, date
 
-start_date = date(2020, 5, 1)
-end_date = date(2020, 5, 5)
-table_route = 'aggs_v2.creation_measures$' 
+start_date = date(2020, 4, 26)
+end_date = date(2020, 4, 27)
+table_route = 'aggs_v2.units_answers$' 
 commend_template = 'bq rm -f '
 
 def daterange(start_date, end_date):
@@ -15,11 +15,11 @@ def main():
 #first stage we gonna delete partitions with for loop according to calender inputs.
 
     for single_date in daterange(start_date, end_date):
-        print(commend_template + table_route + single_date.strftime("%Y%m%d"))
-        # os.system("pwd")
+        tmp = commend_template + "'" + table_route + single_date.strftime("%Y%m%d") + "'"
+        # print(tmp)
+        os.system(tmp)
 
 #second stage we upload new data from side "view" table 
-
 
 
 if __name__ == "__main__":
